335e7977f58c18c9b49a47487c3a4780ed31053d
CodingWizard2001 <goswami.rahul1002@gmail.com>
2024-10-10 06:04:09 +0530

    Basic Structure Complete

---
+++ Assests/JavaScript/script.js
@@ -0,0 +1,9 @@
+// Swiper JavaScript
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30, 
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+    }
+});
