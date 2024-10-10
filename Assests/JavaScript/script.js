7ef2648916006e92f39e5eab08d648e46645aa83
CodingWizard2001 <goswami.rahul1002@gmail.com>
2024-10-10 23:49:47 +0530

    All Set need to responsive

--- Assests/JavaScript/script.js
+++ Assests/JavaScript/script.js
@@ -1,3 +1,24 @@
+// Add shadow on nav bar
+window.addEventListener("scroll", function () {
+    let navbar = document.getElementById("header");
+    if (window.scrollY > 50) navbar.classList.add("shadow");
+    else navbar.classList.remove("shadow");
+});
+
+// Smooth Scrolling
+document.querySelectorAll('.navBar li a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+      
+        targetSection.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+  
+
 // Swiper JavaScript
 let swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
