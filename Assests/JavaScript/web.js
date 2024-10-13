b75030b89b1b73f11d0ed2b7b89dbec412f7bffe
CodingWizard2001 <goswami.rahul1002@gmail.com>
2024-10-13 14:05:13 +0530

    Stop watch and slider added

---
+++ Assests/JavaScript/web.js
@@ -0,0 +1,19 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+    },
+    breakpoints:{
+        500:{
+            slidesPerView: 1,
+        },
+        768:{
+            slidesPerView: 2,
+        },
+        1024:{
+            slidesPerView: 3,
+        },
+    }
+});
